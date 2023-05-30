ae7fe2c50bc57a93c9141ac8829f03d8dbdc4ba9
Tal Jacob <taljacob2@gmail.com>
2023-05-30 12:20:27 +0300

    Add dotenv
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 global.config = require("./config.json");
+const dotenv = require('dotenv');
 const express = require("express");
 const cors = require("cors");
 const meetingsController = require("./controllers-layer/meetings-controller");
 const teamsController = require("./controllers-layer/teams-controller");
 
 const server = express();
+
+// Configure environment variables.
+dotenv.config()
 
 server.use(cors());
 server.use(express.json());
