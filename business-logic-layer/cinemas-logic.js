03dc1bc129889733e04efec91874d86616903eab
Tal Jacob <taljacob2@gmail.com>
2023-05-30 13:13:20 +0300

    update cinemas controller and logic
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

---
+++ business-logic-layer/cinemas-logic.js
@@ -0,0 +1,49 @@
+const dal = require("../data-access-layer/dal");
+
+async function getAllCinemasAsync() {
+    const sql = `SELECT * FROM cinemas`;
+    
+    const cinemas = await dal.executeAsync(sql);
+    return cinemas;
+}
+
+async function getMeetingsByTeamAsync(teamId) {
+    const sql = `SELECT meetingId, T.team, startDate, endDate, description, room
+                 FROM meetings AS M LEFT JOIN teams AS T
+                 ON M.teamId = T.teamId
+                 WHERE T.teamId = ${teamId}`;
+    const meetings = await dal.executeAsync(sql);
+    return meetings;
+}
+
+
+
+async function addMeetingAsync(meeting) {
+    const sql = `INSERT INTO meetings VALUES (DEFAULT,
+                 '${meeting.teamId}',
+                 '${meeting.startDate}',
+                 '${meeting.endDate}',
+                 '${meeting.description}',
+                 '${meeting.room}'
+                  );`
+    
+    const info = await dal.executeAsync(sql);
+    meeting.meetingId = info.insertId;
+
+    meeting.team = await getTeamAsync(meeting.teamId);
+    return meeting;
+}
+
+// Internal Function
+async function getTeamAsync(teamId) {
+    const sql = `SELECT team FROM teams WHERE teamId = ${teamId}`;
+    const team = await dal.executeAsync(sql);
+    return team[0].team;
+}
+
+module.exports = {
+    getAllMeetingsAsync,
+    getMeetingsByTeamAsync,
+    getTeamAsync,
+    addMeetingAsync
+}
---
+++ controllers-layer/cinemas-controller.js
@@ -0,0 +1,43 @@
+const express = require("express");
+
+const meetingsLogic = require("../business-logic-layer/cinemas-logic");
+
+const router = express.Router();
+
+// Get all
+router.get("/", async (request, response) => {
+    try {
+        const cinemas = await meetingsLogic.getAllCinemasAsync();
+        response.json(cinemas);
+    }
+    catch (err) {
+        response.status(500).send(err.message);
+    }
+});
+
+// Add Meeting
+router.post("/", async (request, response) => {
+    try {
+        const meeting = request.body;
+        const addedMeeting = await meetingsLogic.addMeetingAsync(meeting);
+        response.status(201).send(addedMeeting);
+    }
+    catch (err) {
+        response.status(500).send(err.message);
+    }
+});
+
+// Get Meetings by Team Id
+router.get("/:teamId", async (request, response) => {
+    try {
+        const teamId = +request.params.teamId;
+        const meetings = await meetingsLogic.getMeetingsByTeamAsync(teamId);
+        response.json(meetings);
+    }
+    catch (err) {
+        response.status(500).send(err.message);
+    }
+});
+
+
+module.exports = router;
