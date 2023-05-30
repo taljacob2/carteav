a45679e04766a07e02407d2ccb8d06cd142722a9
Tal Jacob <taljacob2@gmail.com>
2023-05-30 23:11:51 +0300

    Add Aproval To `approved`
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- controllers-layer/logs-controller.js
+++ controllers-layer/logs-controller.js
@@ -25,33 +25,12 @@ router.put("/updateLogValue/declined/:logId", async (request, response) => {
     }
 });
 
-// router.put("/updateLogValue/declined", async (request, response) => {
-//     try {
-//         const requestBody = request.body;
-//         const logs = await logsLogic.updateLogValueAsync(requestBody.logId, "admin declined");
-//         response.json(logs);
-//     }
-//     catch (err) {
-//         response.status(500).send(err.message);
-//     }
-// });
-
-// router.put("updateLogValue/approved", async (request, response) => {
-//     try {
-//         const requestBody = request.body;
-//         const logs = await logsLogic.updateLogValueAsync(requestBody.logId, "admin approved");
-//         response.json(logs);
-//     }
-//     catch (err) {
-//         response.status(500).send(err.message);
-//     }
-// });
-
 router.put("/updateLogValue/approved/:logId", async (request, response) => {
     try {
         const logId = +request.params.logId;
         console.log(logId);
         const logs = await logsLogic.updateLogValueAsync(logId, "admin approved");
+        logs = await updateLogAsApprovedByLogIdAsync(logId)
         response.json(logs);
     }
     catch (err) {
