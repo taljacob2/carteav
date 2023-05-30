ee2550213e9147376ba2919e6c2c717e958ad2de
Tal Jacob <taljacob2@gmail.com>
2023-05-30 13:18:58 +0300

    Fix Exports
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- business-logic-layer/users-logic.js
+++ business-logic-layer/users-logic.js
@@ -7,43 +7,18 @@ async function getAllUsersAsync() {
     return users;
 }
 
-async function getMeetingsByTeamAsync(teamId) {
-    const sql = `SELECT meetingId, T.team, startDate, endDate, description, room
-                 FROM meetings AS M LEFT JOIN teams AS T
-                 ON M.teamId = T.teamId
-                 WHERE T.teamId = ${teamId}`;
-    const meetings = await dal.executeAsync(sql);
-    return meetings;
-}
-
-
-
-async function addMeetingAsync(meeting) {
-    const sql = `INSERT INTO meetings VALUES (DEFAULT,
-                 '${meeting.teamId}',
-                 '${meeting.startDate}',
-                 '${meeting.endDate}',
-                 '${meeting.description}',
-                 '${meeting.room}'
+async function addUserAsync(user) {
+    const sql = `INSERT INTO users VALUES (DEFAULT,
+                 '${user.username}'
                   );`
     
     const info = await dal.executeAsync(sql);
-    meeting.meetingId = info.insertId;
+    user.id = info.insertId;
 
-    meeting.team = await getTeamAsync(meeting.teamId);
-    return meeting;
-}
-
-// Internal Function
-async function getTeamAsync(teamId) {
-    const sql = `SELECT team FROM teams WHERE teamId = ${teamId}`;
-    const team = await dal.executeAsync(sql);
-    return team[0].team;
+    return user;
 }
 
 module.exports = {
-    getAllMeetingsAsync,
-    getMeetingsByTeamAsync,
-    getTeamAsync,
-    addMeetingAsync
+    getAllUsersAsync,
+    addUserAsync
 }
