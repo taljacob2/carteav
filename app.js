bec29549e53f80b2636c23d48a3bec275f185207
Tal Jacob <taljacob2@gmail.com>
2023-10-19 03:02:54 +0300

    Add Production Database And Reorder Docker-Compose
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const dotenv = require('dotenv');
+const dotenvExpand = require('dotenv-expand')
 const express = require("express");
 const cors = require("cors");
 const cinemasController = require("./controllers-layer/cinemas-controller");
@@ -9,7 +10,7 @@ const threadDeleteSeatAfterTimer = require("./init-layer/thread-delete-seat-afte
 const server = express();
 
 // Configure environment variables.
-dotenv.config()
+dotenvExpand.expand(dotenv.config())
 
 server.use(cors());
 server.use(express.json());
@@ -26,7 +27,7 @@ server.use("/api/cinemas", cinemasController);
 server.use("/api/users", usersController);
 server.use("/api/logs", logsController);
 
-server.listen(process.env.NODE_LOCAL_PORT, () => console.log("Listening..."));
+server.listen(process.env.NODE_PORT, () => console.log("Listening..."));
 
 // Start thread.
 setInterval(threadDeleteSeatAfterTimer.scanForOldUnaprovedSeatsAndDeleteThem, 1000 * 5)
